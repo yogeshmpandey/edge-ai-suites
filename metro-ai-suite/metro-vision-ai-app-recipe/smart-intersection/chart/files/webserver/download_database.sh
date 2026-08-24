465ad78e92c407fc9044b8753e990ad4f3f4d6b6
Ashish Jagadish <ashish.jagadish@intel.com>
2026-08-24 14:25:37 +0530

    Revert proxy workaround (#3862)

--- metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/files/webserver/download_database.sh
+++ metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/files/webserver/download_database.sh
@@ -6,7 +6,7 @@ else
   echo "File does NOT exist: /data/smart-intersection-ri.tar.bz2"
   echo "Downloading file from GitHub..."
   apk add --no-cache wget
-  wget --no-check-certificate -O /data/smart-intersection-ri.tar.bz2 "{{ .Values.externalUrls.githubRepo }}/raw/refs/tags/{{ .Values.version.release }}/metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/src/webserver/smart-intersection-ri.tar.bz2"
+  wget -O /data/smart-intersection-ri.tar.bz2 "{{ .Values.externalUrls.githubRepo }}/raw/refs/tags/{{ .Values.version.release }}/metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/src/webserver/smart-intersection-ri.tar.bz2"
   if [ $? -eq 0 ] && [ -s /data/smart-intersection-ri.tar.bz2 ]; then
     echo "File downloaded successfully to /data/smart-intersection-ri.tar.bz2"
   else
